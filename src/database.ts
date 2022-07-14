b9d5cfead2c2609bdcbb1d9baec98d402a77eff9
E.W.Ayers <edward.ayers@outlook.com>
2022-07-14 17:55:12 +0100

    desperation

--- src/database.ts
+++ src/database.ts
@@ -2,21 +2,33 @@
 import {Kafka as Kafka} from "https://deno.land/x/kafkasaur@v0.0.7/index.ts"
 import { CallInfo } from "./types.ts";
 
-const kafka = new Kafka({
-    clientId: 'lean-chat-server',
-    brokers: [Deno.env.get('CONFLUENT_BOOTSTRAP_SERVER')!],
-    ssl: true,
-    connectionTimeout: 10000,
-    authenticationTimeout: 10000,
-    sasl: {
-        mechanism: 'plain',
-        username: Deno.env.get('CONFLUENT_API_KEY')!,
-        password: Deno.env.get('CONFLUENT_API_SECRET')!,
+let k : any = undefined
+
+function init() {
+    if (k) {
+        return k
     }
-})
+    const kafka = new Kafka({
+        clientId: 'lean-chat-server',
+        brokers: [Deno.env.get('CONFLUENT_BOOTSTRAP_SERVER')!],
+        ssl: true,
+        connectionTimeout: 10000,
+        authenticationTimeout: 10000,
+        sasl: {
+            mechanism: 'plain',
+            username: Deno.env.get('CONFLUENT_API_KEY')!,
+            password: Deno.env.get('CONFLUENT_API_SECRET')!,
+        }
+    })
+    const producer = kafka.producer()
+    k = {kafka, producer}
+    return k
+}
+
 
 export async function logCall(info: CallInfo) {
     try {
+        let {producer} = init()
         console.log('connecting')
         await producer.connect();
         console.log('sending')
@@ -32,5 +44,4 @@ export async function logCall(info: CallInfo) {
     }
 }
 
-const producer = kafka.producer();
 
